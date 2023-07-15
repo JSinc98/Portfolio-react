c22a4e6c477fa4c8ced0cf78d96f4460b1f7d425
Joshua Sinclair <josh.r.sinclair@gmail.com>
2023-07-14 22:01:20 -0400

    attempting to fix styling

--- myportfolio/src/components/pages/home.js
+++ myportfolio/src/components/pages/home.js
@@ -2,11 +2,14 @@ import React from 'react';
 
 export default function Home() {
   return (
-    <div>
-      <h1>Joshua Sinclair</h1>
+    <div style={{backgroundImage: 'url(${"/images/starrybg.jpg"})',
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover"
+    }}>
+      <h1>Welcome to my portfolio!</h1>
       <img src='../../images/weddinglightsaber.jpg' class="img-fluid" alt="Responsive image" style={{width:'300px', height:'300px'}}></img>
       <p>
-       I'm a 36 year old web developer with a passion for high quality work and helping others. 
+       My name is Joshua Sinclair. I'm a 36 year old web developer from Ottawa with a passion for helping others, working hard and having some fun along the way. I have been in web development since January 2023 and am constantly learning and improving at web development. Before getting into this field I worked as a paramedic in Nova Scotia, which I feel gave me an excellent lesson in time management and working under pressure.
       </p>
     </div>
   );
--- myportfolio/src/components/portfoliocontainer.js
+++ myportfolio/src/components/portfoliocontainer.js
@@ -27,6 +27,7 @@ export default function PortfolioContainer() {
 
   return (
     <div>
+      <h1>Joshua Sinclair</h1>
       {/* We are passing the currentPage from state and the function to update it */}
       <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
       {/* Here we are calling the renderPage method which will return a component  */}
